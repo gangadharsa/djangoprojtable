e5733f13de9441cca47354e9f02834977ac8739e
gangadharsa <gangadharsahoo1991@gmail.com>
2019-04-10 10:00:03 +0530

    Initial commit

---
+++ dtsapptable/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DtsapptableConfig(AppConfig):
+    name = 'dtsapptable'
---
+++ dtsapptable/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def input(request):
+    return render(request,'input.html')
+def display(request):
+    i=int(request.GET["t1"])
+    j=int(request.GET["t2"])
+    k=range(1,j+1)
+    mydic={"tb":i,"values":k}
+    return render(request,'display.html',mydic)
